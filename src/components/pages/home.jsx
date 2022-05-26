7e0c4bb47bd49447a5b5ff7679ae2598f7f8c047
JoshuaPhillips21 <joshua.t.phillips21@gmail.com>
2022-05-25 22:40:37 -0400

    added sign up modal, added home page background image

--- src/components/pages/home.jsx
+++ src/components/pages/home.jsx
@@ -1,26 +1,33 @@
-import React, { Component } from 'react'
-import HomeImage from '../images/Vitruvian_Man.jpg'
+import React, { useState } from "react";
+// import HomeImage from "../images/Vitruvian_Man.jpg";
+import SignUp from "../forms/sign-up";
 
-export default class Home extends Component {
-  render() {
-    return (
-      <div>
-        <div className="title home">
-          <h1>Home</h1>
-        </div>
-        <div>
-          <img src={HomeImage} alt="oh no" />
-        </div>
+export default function Home() {
+  const [openModal, setOpenModal] = useState(false);
 
-        <div className="btn-wrapper">
-          <button className="btn" type="submit">
-            Sign Up
-          </button>
-          <button className="btn" type="submit">
-            Log In
-          </button>
-        </div>
+  return (
+    <div className="home-page">
+      <div className="title home">
+        <h1>Home</h1>
       </div>
-    )
-  }
+      {/* <div className="home-image">
+        <img src={HomeImage} alt="oh no" />
+      </div> */}
+
+      <div className="btn-wrapper">
+        <button
+          className="btn openModal"
+          onClick={() => {
+            setOpenModal(true);
+          }}
+        >
+          Sign Up
+        </button>
+        {openModal && <SignUp closeModal={setOpenModal} />}
+        <button className="btn" type="submit">
+          Log In
+        </button>
+      </div>
+    </div>
+  );
 }
