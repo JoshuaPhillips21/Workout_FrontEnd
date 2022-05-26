5dbf7c679d18216c81cf37fb565f4a0ef071e8b3
JoshuaPhillips21 <joshua.t.phillips21@gmail.com>
2022-05-25 23:47:25 -0400

    removed margin, added background color to navigation bar

--- src/components/navigation/navigation-container.jsx
+++ src/components/navigation/navigation-container.jsx
@@ -1,30 +1,28 @@
-import React from 'react'
-import { A } from 'hookrouter'
+import React from "react";
+import { A } from "hookrouter";
 
 const NavigationComponent = () => {
   return (
     <div className="nav-wrapper">
-      <div className="left-side">
-        <div className="nav-link-wrapper">
-          <A className="link" href="/">
-            Home
-          </A>
-          <A className="link" href="/arms">
-            Arms
-          </A>
-          <A className="link" href="/back">
-            Back
-          </A>
-          <A className="link" href="/chest">
-            Chest
-          </A>
-          <A className="link" href="/legs">
-            Legs
-          </A>
-        </div>
+      <div className="nav-link-wrapper">
+        <A className="link" href="/">
+          Home
+        </A>
+        <A className="link" href="/arms">
+          Arms
+        </A>
+        <A className="link" href="/back">
+          Back
+        </A>
+        <A className="link" href="/chest">
+          Chest
+        </A>
+        <A className="link" href="/legs">
+          Legs
+        </A>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default NavigationComponent
+export default NavigationComponent;
